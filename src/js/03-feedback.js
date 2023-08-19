6cf099f700bfd52ab62897eaf7cae2678cbe5ed7
Kaulits <lehgran@gmail.com>
2023-08-19 16:41:29 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
+};
+
+// Handle form 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const LOCAL_STORAGE = 'feedback-form-state';
+saveData();
+
+function onFormSubmit(e) {
+   e.preventDefault();
+  const formDATA = {
+    email: refs.email.value,
+    message: refs.textarea.value,
+  }
+    console.log(formDATA);
+    refs.form.reset();
+  localStorage.removeItem(LOCAL_STORAGE);
+}
+
+
+function onTextareaInput(e) {
+    const formDATA = {
+    email: refs.email.value,
+    message: refs.textarea.value,
+    }
+    
+    localStorage.setItem(LOCAL_STORAGE, JSON.stringify(formDATA));
+}
+
+function saveData() {
+    const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE));
+    console.log(savedData);
+    if (savedData) {
+        refs.email.value = savedData.email; 
+        refs.textarea.value = savedData.message;
+    }
+} 
